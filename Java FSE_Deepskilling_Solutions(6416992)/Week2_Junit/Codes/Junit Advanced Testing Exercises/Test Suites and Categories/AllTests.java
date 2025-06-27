357a13630eab311f63e4222f8f70369b801a99b2
yerlam sai deepika <122979379+deepikareddy234@users.noreply.github.com>
2025-06-27 13:36:45 +0530

    Delete Java FSE_Deepskilling_Solutions(6416992)/Week2_Junit directory

--- Java FSE_Deepskilling_Solutions(6416992)/Week2_Junit/Codes/Junit Advanced Testing Exercises/Test Suites and Categories/AllTests.java
+++
@@ -1,9 +0,0 @@
-package com.advexample.junitparamdemo;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectClasses({ AppTest.class, MathUtilsTest.class })
-public class AllTests {
-    // No code needed here
-}
--- Java FSE_Deepskilling_Solutions(6416992)/Week2_Junit/Codes/Junit Spring Test Exercises/Test Custom Repository Query/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.junitspring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // 🔥 Custom Query Method
-    List<Employee> findByName(String name);
-}
