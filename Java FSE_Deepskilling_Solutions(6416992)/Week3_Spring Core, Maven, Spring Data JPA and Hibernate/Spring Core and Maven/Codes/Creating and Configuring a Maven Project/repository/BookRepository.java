f688fdb70caf18520b5e7a5b1ea9380cbb45224e
yerlam sai deepika <122979379+deepikareddy234@users.noreply.github.com>
2025-07-04 11:12:45 +0530

    Delete Java FSE_Deepskilling_Solutions(6416992)/Week3_Spring Core, Maven, Spring Data JPA and Hibernate directory

--- Java FSE_Deepskilling_Solutions(6416992)/Week3_Spring Core, Maven, Spring Data JPA and Hibernate/Spring Core and Maven/Codes/Creating and Configuring a Maven Project/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void saveBook(String bookName) {
-        System.out.println("📚 BookRepository: Saving book - " + bookName);
-    }
-}
